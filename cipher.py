dff374082356cd18ae27bbc500b6713ad2e13b63
Michael Gaddis <mgaddis88@gmail.com>
2023-08-06 04:54:51 -0400

    Caesar Cipher assignment

--- cipher.py
+++ cipher.py
@@ -1,1 +1,21 @@
-# add your code here
+letter_dict = {
+    "a": "f", "b": "g", "c": "h", "d": "i", "e": "j",
+    "f": "k", "g": "l", "h": "m", "i": "n", "j": "o",
+    "k": "p", "l": "q", "m": "r", "n": "s", "o": "t",
+    "p": "u", "q": "v", "r": "w", "s": "x", "t": "y",
+    "u": "z", "v": "a", "w": "b", "x": "c", "y": "d", "z": "e"
+}
+
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
+
+encrypted_chars = []
+for char in sentence:
+    if char in letter_dict:
+        encrypted_chars.append(letter_dict[char])
+    else:
+        encrypted_chars.append(char)
+        
+encrypted_sentence = "".join(encrypted_chars)
+print("The encrypted sentence is: ", encrypted_sentence)
+
